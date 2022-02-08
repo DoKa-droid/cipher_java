f63ad76c28cc8ed49d8cd79e18ac3c40cf39ef0a
DoKa-droid <77926375+DoKa-droid@users.noreply.github.com>
2022-02-08 12:01:04 +0700

    Update caesar.java

--- caesar.java
+++ caesar.java
@@ -2,16 +2,23 @@ import java.util.Scanner;
 
 public class caesar {
     public static void main(String[] args) {
-        char operation = args[0].charAt(1);
-        String str = args[1];
-        int offset = Integer.parseInt(args[3]);
+        if (args.length == 4) {
+            char operation = args[0].charAt(1);
+            String str = args[1];
+            int offset = Integer.parseInt(args[3]);
 
-        String result = cipher(operation, str, offset);
-        System.out.println(result);
+            String result = cipher(operation, str, offset);
+            System.out.println(result);
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Press Any Key To Continue...");
-        in.nextLine();
+            Scanner in = new Scanner(System.in);
+            System.out.println("Press Any Key To Continue...");
+            in.nextLine();
+        }
+        else{
+            System.out.println("Wrong input line" +
+                    "\nExample of input: -e/-d \"sentence to encrypt/decrypt\" -o offset_number" +
+                    "\n-e - encrypt\n-d - decrypt\n-o - offset");
+        }
     }
 
     public static String cipher(char operation, String str, int offset){
