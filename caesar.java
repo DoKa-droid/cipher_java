a53e2e7dfcadf4d7086d5ead2d487ed538cc8356
DoKa-droid <77926375+DoKa-droid@users.noreply.github.com>
2022-02-08 17:27:43 +0700

    Update caesar.java

--- caesar.java
+++ caesar.java
@@ -1,21 +1,37 @@
-import java.util.Scanner;
-
 public class caesar {
     public static void main(String[] args) {
+        char operation = 'a';
+        String str = "";
+        int offset = -1;
         if (args.length == 4) {
-            char operation = args[0].charAt(1);
-            String str = args[1];
-            int offset = Integer.parseInt(args[3]);
+            if (args[0].charAt(1) == 'e' || args[0].charAt(1) == 'd') {
+                operation = args[0].charAt(1);
+                str = args[1];
+                offset = Integer.parseInt(args[3]);
+            }
+            else if (args[2].charAt(1) == 'e' || args[2].charAt(1) == 'd') {
+                operation = args[2].charAt(1);
+                str = args[3];
+                offset = Integer.parseInt(args[1]);
+            }
 
-            String result = cipher(operation, str, offset);
-            System.out.println(result);
-
-            Scanner in = new Scanner(System.in);
-            System.out.println("Press Any Key To Continue...");
-            in.nextLine();
+            if (operation != 'a' && str != "" && offset > 0) {
+                String result = cipher(operation, str, offset);
+                System.out.println(result);
+            }
+            else {
+                System.out.println("Wrong arguments" +
+                        "\nExample of input: -e/-d \"sentence to encrypt/decrypt\" -o offset_number" +
+                        "\n-e - encrypt\n-d - decrypt\n-o - offset");
+            }
         }
-        else{
-            System.out.println("Wrong input line" +
+        else if (args.length < 4){
+            System.out.println("Too few arguments" +
+                    "\nExample of input: -e/-d \"sentence to encrypt/decrypt\" -o offset_number" +
+                    "\n-e - encrypt\n-d - decrypt\n-o - offset");
+        }
+        else {
+            System.out.println("Too many arguments" +
                     "\nExample of input: -e/-d \"sentence to encrypt/decrypt\" -o offset_number" +
                     "\n-e - encrypt\n-d - decrypt\n-o - offset");
         }
